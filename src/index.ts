6d5eaacf003e435400589e70aff6a9ebba656f6e
nikitabhairavi <nawar001@ucr.edu>
2024-07-13 21:45:41 -0500

    changes

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req:Request, res:Response) => {
-    res.send('First Express Project');
+    res.send('First Express Project TS');
 });
 
 app.listen(port, () => {
